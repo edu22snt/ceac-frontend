93bdf55dd9754280b95aa34f1b027fbd63279e9e
Eduardo Monteiro de Andrade <edu22snt@gmail.com>
2026-05-08 15:17:51 -0300

    ajustes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VisitanteService } from '../../services/visitante/visitante.service';
+import { HttpResponse } from '@angular/common/http';
+import { IVisitante } from '../../entities/visitante';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  visitantes: IVisitante[] = [];
+  displayedVisitantesColumns: string[] = ['nome', 'unidade', 'veiculo'];
+  displayedMudancasColumns: string[] = ['nome', 'unidade', 'veiculo'];
+
+  constructor(
+    private service: VisitanteService
+  ) { }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.service.recuperarListaVisitantesDiario().subscribe({
+      next: (data) => {
+        this.visitantes = data;
+      },
+      error: (error) => {
+        console.error('Erro ao carregar a lista de veículos', error);
+      }
+    });
+  }
 
 }
+  
--- src/app/services/visitante/visitante.service.ts
+++ src/app/services/visitante/visitante.service.ts
@@ -38,8 +38,11 @@ export class VisitanteService {
 
   findAllNotPage(): Observable<IVisitante[]> {
     return this.http.get<IVisitante[]>(`${this.domain}${this.resourceUrl}/findAllNotPage`);
-  } 
+  }
 
+  recuperarListaVisitantesDiario(): Observable<IVisitante[]> {
+    return this.http.get<IVisitante[]>(`${this.domain}${this.resourceUrl}/recuperarListaVisitantesDiario`);
+  } 
 
   searchByKeyword(param: string, page: number = 0, size: number = 10): Observable<EntityArrayResponseType> {
     return this.http.get<IVisitante[]>(
